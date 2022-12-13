ecd826b83857951357e558f4439f465fe422a47e
YasmineHannouf <yasmina.hannouf.2018@gmail.com>
2022-12-13 16:31:32 +0200

    condition

---
+++ problem2.js
@@ -0,0 +1,10 @@
+function show(){
+    var a=document.getElementById("Name").value;
+    if ((a=="Bob") || (a=="Alex")) {
+        alert("hello "+a)
+    }
+
+    else{
+        alert("hello Stranger") 
+    }
+}
